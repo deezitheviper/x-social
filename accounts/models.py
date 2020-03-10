f40e2fd01fb20054df2293bdc673dd57b81d6ddd
Epic david <toonerdrill@gmail.com>
2020-03-10 10:48:05 -0700

    Added avatar to user model

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,7 @@ from django.contrib import auth
 
 # Create your models here.
 class User(auth.models.User,auth.models.PermissionsMixin):
+    avatar = models.ImageField(upload_to='media/avatar/', default='media/avatar/icons8-administrator-male-50.png') 
 
     def __str__(self):
         return "@{}".format(self.username)
