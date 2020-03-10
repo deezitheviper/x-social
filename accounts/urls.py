6b4f69bb3c6f843cf12db848f405cb87537db653
Epic david <toonerdrill@gmail.com>
2020-03-10 10:51:20 -0700

    updated account url for image upload

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 app_name = 'account'
 
 urlpatterns = [
@@ -8,5 +11,5 @@ urlpatterns = [
     path('signup/', views.signup.as_view(), name='signup'),
     path('logout/', auth_views.LogoutView.as_view(),name='logout'),
     path('profile/', views.profileView.as_view(), name='profile')
-]
+]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
